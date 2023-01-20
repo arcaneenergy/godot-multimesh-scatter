d49e8db62d94cb578eaceedb63f1178e3390dba2
Nicola Baribeau <n_baribeau@hotmail.com>
2023-01-19 22:47:16 -0500

    Adding many new features.

--- addons/multimesh_scatter/multimesh_scatter.gd
+++ addons/multimesh_scatter/multimesh_scatter.gd
@@ -21,20 +21,27 @@
 @tool
 extends MultiMeshInstance3D
 
-enum PlacementType { BOX, SPHERE }
+enum ScatterType { BOX, SPHERE }
+
+@export_group("Multi Mesh Scatter")
+
+@export_subgroup("Scattering")
 
 ## The number of instances to generate.
-@export_range(0, 10000, 1) var count := 100:
+@export var count := 100:
 	get: return count
 	set(value):
+		if value > 1000:
+			print("MultiMeshScatter: You tried to set a scatter count above 1000, it's probably a mistake. If you truly want to just edit this check out yourself.")
+			return
 		count = value
 		_update()
 
 ## Defines the placement type.
-@export_enum("Box", "Sphere") var placement_type: int = PlacementType.BOX:
-	get: return placement_type
+@export_enum("Box", "Sphere") var scatter_type: int = ScatterType.BOX:
+	get: return scatter_type
 	set(value):
-		placement_type = value
+		scatter_type = value
 
 		if Engine.is_editor_hint():
 			_create_debug_area()
@@ -43,14 +50,14 @@ enum PlacementType { BOX, SPHERE }
 
 ## The placement size of the bounding box.
 ## Enable [code]show_debug_area[/code] to view the size of the bounding box.
-## [br][br] Note: If the [code]placement_type[/code] is set to Sphere,
+## [br][br] Note: If the [code]scatter_type[/code] is set to Sphere,
 ## only the x value will be used to specify the radius of the sphere.
-@export var placement_size := Vector3(10.0, 10.0, 10.0):
-	get: return placement_size
+@export var scatter_size := Vector3(10.0, 10.0, 10.0):
+	get: return scatter_size
 	set(value):
-		placement_size = value.clamp(Vector3.ONE * 0.01, Vector3.ONE * 100.0)
+		scatter_size = value.clamp(Vector3.ONE * 0.01, Vector3.ONE * 100.0)
 		_update()
-
+	
 ## The physics collision mask that the instances should collide with.
 @export_flags_3d_physics var collision_mask := 0x1:
 	get: return collision_mask
@@ -58,7 +65,23 @@ enum PlacementType { BOX, SPHERE }
 		collision_mask = value
 		_update()
 
-@export_group("Offset")
+@export_subgroup("Mesh")
+
+## Setting this value will copy over the MeshInstance's Mesh to the MultiMeshInstance3D.
+## This is just for convenience.
+@export_node_path(MeshInstance3D) var mesh_instance:
+	get: return mesh_instance
+	set(value):
+		if value:
+			var i = get_node(value)
+			if i.mesh:
+				print("MultiMeshScatter: Mesh from MeshInstance added to the MultiMeshInstance3D. You can safely remove it.")
+				multimesh.mesh = i.mesh
+		mesh_instance = value
+
+@export_group("Multi Mesh Placement")
+
+@export_subgroup("Offset")
 
 ## Add an offset to the placed instances.
 @export var offset_position := Vector3(0.0, 0.0, 0.0):
@@ -81,7 +104,7 @@ enum PlacementType { BOX, SPHERE }
 		base_scale = value.clamp(Vector3.ONE * 0.01, Vector3.ONE * 100.0)
 		_update()
 
-@export_group("Random Size")
+@export_subgroup("Random Size")
 
 ## The minimum random size for each instance.
 @export var min_random_size := Vector3(0.75, 0.75, 0.75):
@@ -97,7 +120,7 @@ enum PlacementType { BOX, SPHERE }
 		max_random_size = value.clamp(Vector3.ONE * 0.01, Vector3.ONE * 100.0)
 		_update()
 
-@export_group("Random Rotation")
+@export_subgroup("Random Rotation")
 
 ## Rotate each instance by a random amount between
 ## [code]-random_rotation[/code] and +[code]random_rotation[/code].
@@ -107,17 +130,59 @@ enum PlacementType { BOX, SPHERE }
 		random_rotation = value.clamp(Vector3.ONE * 0.00, Vector3.ONE * 180.0)
 		_update()
 
-@export_group("Advanced parameters")
+@export_group("Multi Mesh Advanced")
 
-## A seed to feed for the random number generator.
-@export_range(0, 10000, 1) var seed := 0:
+@export_subgroup("Vertex Colors")
+
+## If enabled the scattering will only happen where vertex color of the surface below the specified threshold.
+## This has a non-negligible impact on scattering speed but no impact once the scattering is done.
+## This will result in less instances than the set [code]count[/code].
+## (Those instances are actually just scaled to 0)
+@export var use_vertex_colors: bool = false:
+	get: return use_vertex_colors
+	set(value):
+		use_vertex_colors = value
+		_update()
+
+## Scatter threshold for the red channel.
+@export_range(0, 1, 0.01) var r_channel = 1.0:
+	get: return r_channel
+	set(value):
+		r_channel = value
+		_update()
+		
+## Scatter threshold for the green channel.
+@export_range(0, 1, 0.01) var g_channel = 1.0:
+	get: return g_channel
+	set(value):
+		g_channel = value
+		_update()
+		
+## Scatter threshold for the blue channel.
+@export_range(0, 1, 0.01) var b_channel = 1.0:
+	get: return b_channel
+	set(value):
+		b_channel = value
+		_update()
+		
+@export_subgroup("Seed")
+
+## Click to randomize the seed.
+@export var randomize_seed := true:
+	get: return randomize_seed
+	set(value):
+		seed = randi()
+		randomize_seed = false
+		
+## A seed to feed for the random number generator if randomize seed is false.
+@export var seed := 0:
 	get: return seed
 	set(value):
 		seed = value
 		_rng.seed = value
 		_update()
 
-@export_group("Debug")
+@export_subgroup("Debug")
 
 ## Toggle the visibility of the bounding box area.
 @export var show_debug_area := true:
@@ -132,17 +197,14 @@ enum PlacementType { BOX, SPHERE }
 
 var _debug_draw_instance: MeshInstance3D
 var _rng := RandomNumberGenerator.new()
+var _mesh_data_array: Dictionary = {}
 
 @onready var _space: PhysicsDirectSpaceState3D = get_world_3d().direct_space_state
 
 func _init() -> void:
-	if multimesh == null:
-		multimesh = MultiMesh.new()
-		multimesh.transform_format = MultiMesh.TRANSFORM_3D
+	_ensure_has_mm()
 
 func _ready() -> void:
-	_rng.seed = seed
-
 	if Engine.is_editor_hint():
 		if show_debug_area:
 			_create_debug_area()
@@ -151,7 +213,7 @@ func _ready() -> void:
 	else:
 		set_notify_transform(false)
 		set_ignore_transform_notification(true)
-
+	
 	_update()
 
 func _notification(what: int) -> void:
@@ -159,6 +221,12 @@ func _notification(what: int) -> void:
 
 	if NOTIFICATION_TRANSFORM_CHANGED:
 		_update()
+
+func _ensure_has_mm() -> bool:
+	if multimesh == null:
+		multimesh = MultiMesh.new()
+		multimesh.transform_format = MultiMesh.TRANSFORM_3D
+	return multimesh.mesh != null
 
 func _delete_debug_area() -> void:
 	if _debug_draw_instance != null && _debug_draw_instance.is_inside_tree():
@@ -179,10 +247,10 @@ func _create_debug_area() -> void:
 	material.no_depth_test = true
 
 	var mesh: Mesh
-	match placement_type:
-		PlacementType.SPHERE:
+	match scatter_type:
+		ScatterType.SPHERE:
 			mesh = SphereMesh.new()
-		PlacementType.BOX, _:
+		ScatterType.BOX, _:
 			mesh = BoxMesh.new()
 
 	_debug_draw_instance.mesh = mesh
@@ -193,12 +261,12 @@ func _create_debug_area() -> void:
 
 func _update_debug_area_size() -> void:
 	if _debug_draw_instance != null && _debug_draw_instance.is_inside_tree():
-		match placement_type:
-			PlacementType.SPHERE:
-				_debug_draw_instance.mesh.radius = placement_size.x / 2.0
-				_debug_draw_instance.mesh.height = placement_size.x
-			PlacementType.BOX, _:
-				_debug_draw_instance.mesh.size = placement_size
+		match scatter_type:
+			ScatterType.SPHERE:
+				_debug_draw_instance.mesh.radius = scatter_size.x / 2.0
+				_debug_draw_instance.mesh.height = scatter_size.x
+			ScatterType.BOX, _:
+				_debug_draw_instance.mesh.size = scatter_size
 
 func _update() -> void:
 	if !_space: return
@@ -210,35 +278,53 @@ func _update() -> void:
 func scatter() -> void:
 	_rng.state = 0
 	_rng.seed = seed
-
-	multimesh.instance_count = 0
+	
+	if not _ensure_has_mm(): 
+		print("MultiMeshScatter: The MultiMeshInstance3D doesn't have an assigned Mesh. Set it yourself or set a MeshInstance on the MultiMeshScatter so it can copy it for you.")
+		return
+	
 	multimesh.instance_count = count
 
 	for i in range(count):
 		var pos := global_position
 
-		match placement_type:
-			PlacementType.SPHERE:
-				var radius := sqrt(_rng.randf()) * (placement_size.x / 2.0)
+		match scatter_type:
+			ScatterType.SPHERE:
+				var radius := sqrt(_rng.randf()) * (scatter_size.x / 2.0)
 				var theta := _rng.randf_range(0.0, 360.0)
 				pos += Vector3(
 					radius * cos(theta),
 					0.0,
 					radius * sin(theta))
-			PlacementType.BOX, _:
+			ScatterType.BOX, _:
 				pos += Vector3(
-					_rng.randf_range(-placement_size.x / 2.0, placement_size.x / 2.0),
+					_rng.randf_range(-scatter_size.x / 2.0, scatter_size.x / 2.0),
 					0.0,
-					_rng.randf_range(-placement_size.z / 2.0, placement_size.z / 2.0))
+					_rng.randf_range(-scatter_size.z / 2.0, scatter_size.z / 2.0))
 
 		var ray := PhysicsRayQueryParameters3D.create(
-			pos + Vector3.UP * (placement_size.y / 2.0),
-			pos + Vector3.DOWN * (placement_size.y / 2.0),
+			pos + Vector3.UP * (scatter_size.y / 2.0),
+			pos + Vector3.DOWN * (scatter_size.y / 2.0),
 			collision_mask)
 
 		var hit := _space.intersect_ray(ray)
 		if hit.is_empty(): continue
 
+		var iteration_scale = base_scale
+
+		# Vertex color check
+		var mesh = find_mesh(hit.collider)
+		if mesh:
+			var mesh_id = mesh.get_instance_id()
+			if not _mesh_data_array.has(mesh_id):
+				var mdt = MeshDataTool.new()
+				mdt.create_from_surface(mesh.mesh, 0)
+				_mesh_data_array[mesh_id] = mdt
+			var color = _mesh_data_array[mesh_id].get_vertex_color(get_closest_vertex(_mesh_data_array[mesh_id], hit.position))
+			if use_vertex_colors:
+				if not (color.r < r_channel && color.g < g_channel && color.b < b_channel):
+					iteration_scale = 0
+		
 		var t := Transform3D(
 			Basis(
 				hit.normal.cross(global_transform.basis.z),
@@ -250,9 +336,39 @@ func scatter() -> void:
 			.rotated(Vector3.RIGHT, deg_to_rad(_rng.randf_range(-random_rotation.x, random_rotation.x) + offset_rotation.x))\
 			.rotated(Vector3.UP, deg_to_rad(_rng.randf_range(-random_rotation.y, random_rotation.y) + offset_rotation.y))\
 			.rotated(Vector3.FORWARD, deg_to_rad(_rng.randf_range(-random_rotation.z, random_rotation.z) + offset_rotation.z))\
-			.scaled(base_scale * Vector3(
+			.scaled(iteration_scale * Vector3(
 				_rng.randf_range(min_random_size.x, max_random_size.x),
 				_rng.randf_range(min_random_size.y, max_random_size.y),
 				_rng.randf_range(min_random_size.z, max_random_size.z)))
 		t.origin = hit.position - global_position + offset_position
 		multimesh.set_instance_transform(i, t)
+
+func get_closest_vertex(mdt: MeshDataTool, pos: Vector3):
+	var closest_dist = INF
+	var closest_vertex = -1
+
+	for v in range(mdt.get_vertex_count()):
+		var v_pos = mdt.get_vertex(v)
+		var tmp = pos.distance_squared_to(v_pos)
+		if (tmp <= closest_dist):
+			closest_dist = tmp
+			closest_vertex = v
+	
+	return closest_vertex
+
+# Search up and down the tree until a mesh is found (dumb)
+func find_mesh(node) -> MeshInstance3D:
+	if node is MeshInstance3D: return node
+	var up = find_mesh_in_parent(node)
+	var down = find_mesh_in_children(node)
+	return down if down else up if up else null
+
+func find_mesh_in_parent(node):
+	var p = node.get_parent()
+	if p == null: return p
+	return p if p is MeshInstance3D else find_mesh_in_parent(p)
+
+func find_mesh_in_children(node):
+	for c in node.get_children():
+		return c if c is MeshInstance3D else find_mesh_in_children(c)
+	return null
